b55158af1892a1387a99590b963f10ff9ba09736
mkowalik92 <mkowalik92@gmail.com>
2018-02-06 21:32:14 -0500

    Update page_handler.php

--- Alpha/page_handler.php
+++ Alpha/page_handler.php
@@ -23,10 +23,25 @@
       curl_setopt($ch,CURLOPT_URL, $url);
       curl_setopt($ch,CURLOPT_POST, 2);
       curl_setopt($ch,CURLOPT_POSTFIELDS, $post_string);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
       $result = curl_exec($ch);
 
       curl_close($ch);
+
+      $parse = str_replace("<br>","",$result);
+      $json_array = json_decode($parse,true);
+
+      if ($json_array["NJITverified"] == 1) {
+        echo "NJIT Login verified!<br>";
+      } else {
+        echo "NJIT Login NOT verified!<br>";
+      }
+      if ($json_array["SQLverified"] == 1) {
+        echo "SQL Login verified!";
+      } else {
+        echo "SQL Login NOT verified!";
+      }
     ?>
   </body>
 
