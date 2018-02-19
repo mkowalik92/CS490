2080531b4f13b163f1845a84c9c2b174b88daf9a
Bill Ngo <Mr.Bill.Ngo@Gmail.com>
2018-02-18 21:54:13 -0500

    Added some code to show how to handle data
    Also, I believe this is suppose to make an AJAX call to your php.
    In that authenticator.php is where you read the JSON from my php and echo instructor / student / none for this script to handle.

--- Beta/Front-end/login-js.js
+++ Beta/Front-end/login-js.js
@@ -1,16 +1,27 @@
 function login() {
+  document.getElementById("wrong_password").innerHTML = ""; // reset on each click
+  
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange=function() {
     if (this.readyState == 4 && this.status == 200) {
-      var responseString = this.responseText;
+      var result = this.responseText;
       // Do something with your response depending on what it is
       // I need to know if the username and password are valid.
       // If not valid then wrong password div gets displayed
       // If valid redirect to proper home page
-      document.getElementById("wrong_password").innerHTML = responseString;
+      
+      // You can check if responseText == "instructor" to redirect to intructor page. Same for student.
+      if(result == "instructor"){
+        // use window.location to redirect to instructor page
+      }else if(result == "student"){
+        // use window.location to redirect to student page
+      }else{        
+        document.getElementById("wrong_password").innerHTML = "Invalid username and password!";
+      }
     }
   }
-  xhr.open("POST", "login_page_handler.php", true);
+  // your authenticator.php should return instructor / student / invalid based on the JSON my php will be sending back to you.
+  xhr.open("POST", "authenticator.php", true);
   xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
   var loginFormData = "username=" + document.getElementById('username').value + "&password=" + document.getElementById('password').value;
   xhr.send(loginFormData);
