a3a7bd7ba4c77aa3ebd64fe2bf3fd7dd8151449c
mkowalik92 <mkowalik92@gmail.com>
2018-02-22 04:01:09 -0500

    Create student_home_page.php

---
+++ Beta/Frontend/student_home_page.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+  echo $_SESSION['username'];
+?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://web.njit.edu/~mk343/cs490/logout.js"></script>
+  </head>
+
+  <body>
+    <h1>Student Home Page</h1>
+    <input type="button" value="Logout" onclick="logout()">
+  </body>
+
+</html>
