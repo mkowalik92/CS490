aad120d24764a5c55181423e486711d8dc274f56
Bill Ngo <Mr.Bill.Ngo@Gmail.com>
2018-03-07 20:22:40 -0500

    Add files via upload
    Added CRUD files

---
+++ Beta/MiddleEnd/crud/users.php
@@ -0,0 +1,20 @@
+<?php
+$ch = curl_init();
+
+$url = "https://web.njit.edu/~mni22/CS490/Beta/crud/users.php";
+
+foreach ($_POST as $key => $value) {
+  $post_items[] = $key . '=' . $value;
+}
+$post_string = implode ('&', $post_items);
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+curl_close($ch);
+
+echo $output;
+
+?>
