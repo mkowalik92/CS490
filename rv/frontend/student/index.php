fced3c23f517ee8452169e0114beae3ce6b425ce
mkowalik92 <mkowalik92@gmail.com>
2018-04-09 00:42:41 -0400

    Create index.php

---
+++ rv/frontend/student/index.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+?>
+<html>
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--link rel="stylesheet" type="text/css" href="login_stylesheet.css">
+    <script src="login_script.js"></script-->
+  </head>
+
+  <body>
+    <h1>Student</h1>
+  </body>
+
+</html>
